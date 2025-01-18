c00441ce5a75c2ace63767af883882860267fe3a
Eugenetanwl3881 <Eugene.tan.wl@gmail.com>
2025-01-18 17:46:22 +0800

    Add encoding

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,6 +1,13 @@
 from flask import Blueprint, render_template, request, jsonify
+import base64
+from openai import OpenAI
 
 main = Blueprint('main', __name__)
+client = OpenAI()
+
+# Function to encode the image
+def encode_image(image_file):
+    return base64.b64encode(image_file.read()).decode("utf-8")
 
 @main.route('/', methods=['GET'])
 def test():
@@ -9,5 +16,27 @@ def test():
 @main.route('/upload', methods=['POST'])
 def upload():
     if 'file' not in request.files:
-        return jsonify({'error': 'No file part'}), 400
-    return jsonify({"status_code": "success"}), 200
+            return jsonify({'error': 'No file part'}), 400
+    file = request.files['file']
+    base64_image = encode_image(file)
+
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": "Is this a rock?",
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"},
+                    },
+                ],
+            }
+        ],
+    )
+
+    return jsonify(response.choices[0])
